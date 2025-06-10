e58f7fcb5158be249625270151cae31cfb29faff
Mouddassar Baig <Baigmou@sheridancollege.ca>
2025-06-10 15:49:15 -0400

    Added names of collaborators

--- src/ca/sheridancollege/project/Player.java
+++ src/ca/sheridancollege/project/Player.java
@@ -8,8 +8,8 @@ package ca.sheridancollege.project;
 /**
  * A class that models each Player in the game. Players have an identifier, which should be unique.
  *
- * @author dancye
- * @author Paul Bonenfant Jan 2020
+ * @author Mouddassar Baig 
+ * @author Valmir Muratovski June 2025
  */
 public abstract class Player {
 
