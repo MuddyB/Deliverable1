5127a4316bb8a326f39c791b71fc832849083e24
Mouddassar Baig <Baigmou@sheridancollege.ca>
2025-06-10 15:48:34 -0400

    Added names of collaborators

--- src/ca/sheridancollege/project/Card.java
+++ src/ca/sheridancollege/project/Card.java
@@ -8,8 +8,8 @@ package ca.sheridancollege.project;
 /**
  * A class to be used as the base Card class for the project. Must be general enough to be instantiated for any Card
  * game. Students wishing to add to the code should remember to add themselves as a modifier.
- *
- * @author dancye
+ * @author Mouddassar Baig 
+ * @author Valmir Muratovski June 2025
  */
 public abstract class Card {
     //default modifier for child classes
