7eeb8c2d76acb4661d06a87fbda6fd81c76ee7b7
Mouddassar Baig <Baigmou@sheridancollege.ca>
2025-07-18 17:27:19 -0400

    main class

---
+++ src/ca/sheridancollege/project/Main.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ca.sheridancollege.project;
+
+/**
+ *
+ * @author Mudaser
+ */
+
+//play the game
+public class Main {
+    public static void main(String[] args) {
+        UnoGame game = new UnoGame("UNO");
+        game.play();
+    }
+}
